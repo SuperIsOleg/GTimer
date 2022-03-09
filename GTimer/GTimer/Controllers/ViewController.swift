721f3d4a37592768105cdba0a1b3ef3bba289ea7
SuperIsOleg <anyplusoleg@mail.ru>
2022-03-10 02:23:20 +0300

    add button Cancel, Pause and Resume.

--- GTimer/GTimer/Controllers/ViewController.swift
+++ GTimer/GTimer/Controllers/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     
     let timerLabel : UILabel = {
         let timerLabel = UILabel()
-        timerLabel.text = "1"
+        timerLabel.text = "05:00"
         timerLabel.font = UIFont.systemFont(ofSize: 50)
         timerLabel.textColor = .white
         timerLabel.numberOfLines = 0
@@ -36,11 +36,25 @@ class ViewController: UIViewController {
         return startButton
     }()
     
+    private let cancelButton : UIButton = {
+        let cancelButton = UIButton()
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.setTitleColor(.white, for: .normal)
+        cancelButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        cancelButton.titleLabel?.numberOfLines = 0
+        cancelButton.titleLabel?.textAlignment = .center
+        cancelButton.backgroundColor = nil
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        return cancelButton
+    }()
+    
+    
     var timer = Timer()
+    var isTimerStarted = false
     
     let shapeLayer = CAShapeLayer()
     
-    var durationTimer = 10
+    var durationTimer = 300
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -50,7 +64,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timerLabel.text = "\(durationTimer)"
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
@@ -61,22 +74,54 @@ class ViewController: UIViewController {
         setConstraints()
         
         startButton.addTarget(self, action: #selector(startButtonTaped), for: .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(cancelButtonTaped), for: .touchUpInside)
     }
     
-    
     @objc func startButtonTaped() {
-        basicAnimation()
+        cancelButton.isEnabled = true
+        cancelButton.alpha = 1.0
+        
+        if !isTimerStarted{
+            basicAnimation()
+            startTimer()
+            isTimerStarted = true
+            startButton.setBackgroundImage(UIImage(named: "PauseButton"), for: .normal)
+            
+        } else {
+            timer.invalidate()
+            isTimerStarted = false
+            startButton.setBackgroundImage(UIImage(named: "ResumeButton"), for: .normal)
+        }
+    }
+    
+    func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func cancelButtonTaped() {
+        cancelButton.isEnabled = false
+        cancelButton.alpha = 0.5
+        timer.invalidate()
+        durationTimer = 300
+        isTimerStarted = false
+        timerLabel.text = "05:00"
+        startButton.setBackgroundImage(UIImage(named: "StartButton"), for: .normal)
     }
     
     @objc func timerAction() {
         durationTimer -= 1
-        timerLabel.text = "\(durationTimer)"
+        timerLabel.text = formatTimer()
         print(durationTimer)
         
         if durationTimer == 0 {
             timer.invalidate()
         }
+    }
+    
+    func formatTimer() -> String {
+        let minutes = Int(durationTimer) / 60 % 60
+        let seconds = Int(durationTimer) % 60
+        return String(format: "%02i:%02i", minutes, seconds)
     }
     
     //MARC: Animation
@@ -132,6 +177,14 @@ extension ViewController {
             startButton.bottomAnchor.constraint(equalTo: cercleTimer.bottomAnchor, constant: 178),
             startButton.heightAnchor.constraint(equalToConstant: 120),
             startButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+        
+        view.addSubview(cancelButton)
+        NSLayoutConstraint.activate([
+            cancelButton.centerXAnchor.constraint(equalTo: startButton.centerXAnchor),
+            cancelButton.topAnchor.constraint(equalTo: startButton.bottomAnchor, constant: 0),
+            cancelButton.heightAnchor.constraint(equalToConstant: 30),
+            cancelButton.widthAnchor.constraint(equalToConstant: 70)
         ])
     }
 }
