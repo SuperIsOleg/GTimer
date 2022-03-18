049ada88e1cd8314845c98ab75b07556c97f6488
SuperIsOleg <anyplusoleg@mail.ru>
2022-03-18 17:44:05 +0300

    add labelWork, labelBreak. Edit viewWork and viewBreak and other.

--- GTimer/GTimer/Controllers/ViewController.swift
+++ GTimer/GTimer/Controllers/ViewController.swift
@@ -10,33 +10,100 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    let cercleTimer : UIImageView = {
+    lazy var viewTimerWorked: UIView = {
+        let viewTimerWorked = UIView()
+        viewTimerWorked.backgroundColor = nil
+        return viewTimerWorked
+    }()
+    
+    lazy var viewTimerBreak: UIView = {
+        let viewTimerBreak = UIView()
+        viewTimerBreak.backgroundColor = nil
+        return viewTimerBreak
+    }()
+    
+    lazy var views = [viewTimerWorked, viewTimerBreak]
+    
+    let cercleTimerWorkImage: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage.init(named: "TimerCircel")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    let timerLabel : UILabel = {
-        let timerLabel = UILabel()
-        timerLabel.text = "05:00"
-        timerLabel.font = UIFont.systemFont(ofSize: 50)
-        timerLabel.textColor = .white
-        timerLabel.numberOfLines = 0
-        timerLabel.textAlignment = .center
-        timerLabel.translatesAutoresizingMaskIntoConstraints = false
-        return timerLabel
+    let cercleTimerBreakImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage.init(named: "TimerCircel")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
-    let pageControl: UIPageControl = {
+    let timerWorkLabel: UILabel = {
+        let timerWorkLabel = UILabel()
+        timerWorkLabel.text = "01:00"
+        timerWorkLabel.font = UIFont.systemFont(ofSize: 50)
+        timerWorkLabel.textColor = .white
+        timerWorkLabel.numberOfLines = 0
+        timerWorkLabel.textAlignment = .center
+        timerWorkLabel.translatesAutoresizingMaskIntoConstraints = false
+        return timerWorkLabel
+    }()
+    
+    let timerBreakLabel: UILabel = {
+        let timerBreakLabel = UILabel()
+        timerBreakLabel.text = "00:30"
+        timerBreakLabel.font = UIFont.systemFont(ofSize: 50)
+        timerBreakLabel.textColor = .white
+        timerBreakLabel.numberOfLines = 0
+        timerBreakLabel.textAlignment = .center
+        timerBreakLabel.translatesAutoresizingMaskIntoConstraints = false
+        return timerBreakLabel
+    }()
+    
+    let workLabel: UILabel = {
+        let workLabel = UILabel()
+        workLabel.text = "Work"
+        workLabel.font = UIFont.systemFont(ofSize: 25)
+        workLabel.textColor = .white
+        workLabel.numberOfLines = 0
+        workLabel.textAlignment = .center
+        workLabel.translatesAutoresizingMaskIntoConstraints = false
+        return workLabel
+    }()
+    
+    let breakLabel: UILabel = {
+        let breakLabel = UILabel()
+        breakLabel.text = "Break"
+        breakLabel.font = UIFont.systemFont(ofSize: 25)
+        breakLabel.textColor = .white
+        breakLabel.numberOfLines = 0
+        breakLabel.textAlignment = .center
+        breakLabel.translatesAutoresizingMaskIntoConstraints = false
+        return breakLabel
+    }()
+    
+    lazy  var pageControl: UIPageControl = {
         let pageControl = UIPageControl()
-        pageControl.numberOfPages = 2
+        pageControl.numberOfPages = views.count
         pageControl.backgroundColor = nil
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         return pageControl
     }()
     
-    private let startButton : UIButton = {
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: view.frame.size.width * CGFloat(views.count), height: view.frame.size.height)
+        for i in 0..<views.count {
+            views[i].frame = CGRect(x: CGFloat(i) * view.frame.size.width, y: 120, width: view.frame.width, height: 400)
+            scrollView.addSubview(views[i])
+        }
+        scrollView.delegate = self
+        return scrollView
+    }()
+    
+    private let startButton: UIButton = {
         let startButton = UIButton()
         startButton.layer.cornerRadius = 20
         startButton.setBackgroundImage(UIImage(named: "StartButton"), for: .normal)
@@ -56,46 +123,32 @@ class ViewController: UIViewController {
         return cancelButton
     }()
     
-    let scrollView = UIScrollView()
-    
     var timer = Timer()
-    var durationTimer = 300
+    var durationTimer = 60
     var isTimerStarted = false
-    let shapeLayer = CAShapeLayer()
+    let shapeLayerWorkTimer = CAShapeLayer()
+    let shapeLayerBreakTimer = CAShapeLayer()
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.animationCircular()
-        
-        if scrollView.subviews.count == 2 {
-            configureScrollView()
-        }
-    }
-    
-    func configureScrollView() {
-        scrollView.contentSize = CGSize(width: view.frame.size.width*2, height: scrollView.frame.size.height)
-        scrollView.isPagingEnabled = true
-        let colors: [UIColor] = [.green, .red]
-        for x in 0..<2 {
-            let page = UIView(frame: CGRect(x: CGFloat(x) * view.frame.size.width, y: 0, width: view.frame.self.width, height:  scrollView.frame.size.height))
-            page.backgroundColor = colors[x]
-            scrollView.addSubview(page)
-        }
+        self.animationCircularWorkTimer()
+        self.animationCircularBreakTimer()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pageControl.addTarget(self, action: #selector(pageControlDidChange(_ :)), for: .valueChanged)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.delegate = self
-        view.addSubview(scrollView)
-        view.addSubview(pageControl)
-        
+
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor(named: "LightBlue")!.cgColor,
                                 UIColor(named: "DarkBlue")!.cgColor]
         view.layer.addSublayer(gradientLayer)
+        
+        scrollView.frame = view.bounds
+        view.addSubview(scrollView)
+        
+        pageControl.addTarget(self, action: #selector(pageControlDidChange(_ :)), for: .valueChanged)
+        view.addSubview(pageControl)
         
         setConstraints()
         
@@ -133,19 +186,19 @@ class ViewController: UIViewController {
         cancelButton.isEnabled = false
         cancelButton.alpha = 0.5
         timer.invalidate()
-        durationTimer = 300
+        durationTimer = 60
         isTimerStarted = false
-        timerLabel.text = "05:00"
+        timerWorkLabel.text = "01:00"
         startButton.setBackgroundImage(UIImage(named: "StartButton"), for: .normal)
-        
     }
     
     @objc func timerAction() {
         durationTimer -= 1
-        timerLabel.text = formatTimer()
+        timerWorkLabel.text = formatTimer()
         
         if durationTimer == 0 {
             timer.invalidate()
+            timerWorkLabel.text = "01:00"
         }
     }
     
@@ -157,20 +210,36 @@ class ViewController: UIViewController {
     
     //MARC: Animation
     
-    func animationCircular() {
+    func animationCircularWorkTimer() {
         
-        let center = CGPoint(x: cercleTimer.frame.width / 2, y: cercleTimer.frame.height / 2)
+        let center = CGPoint(x: cercleTimerWorkImage.frame.width / 2, y: cercleTimerWorkImage.frame.height / 2)
         let endAngle = (-CGFloat.pi / 2)
         let startAngle = 2 * CGFloat.pi + endAngle
-        let circularPatch = UIBezierPath(arcCenter: center, radius: 92, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        let circularPatch = UIBezierPath(arcCenter: center, radius: 97, startAngle: startAngle, endAngle: endAngle, clockwise: false)
         
-        shapeLayer.path = circularPatch.cgPath
-        shapeLayer.lineWidth = 3.5
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.strokeEnd = 1
-        shapeLayer.lineCap = CAShapeLayerLineCap.round
-        shapeLayer.strokeColor = UIColor(named: "TimerColor")!.cgColor
-        cercleTimer.layer.addSublayer(shapeLayer)
+        shapeLayerWorkTimer.path = circularPatch.cgPath
+        shapeLayerWorkTimer.lineWidth = 3.5
+        shapeLayerWorkTimer.fillColor = UIColor.clear.cgColor
+        shapeLayerWorkTimer.strokeEnd = 1
+        shapeLayerWorkTimer.lineCap = CAShapeLayerLineCap.round
+        shapeLayerWorkTimer.strokeColor = UIColor(named: "TimerColorWork")!.cgColor
+        cercleTimerWorkImage.layer.addSublayer(shapeLayerWorkTimer)
+    }
+    
+    func animationCircularBreakTimer() {
+        
+        let center = CGPoint(x: cercleTimerBreakImage.frame.width / 2, y: cercleTimerBreakImage.frame.height / 2)
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let circularPatch = UIBezierPath(arcCenter: center, radius: 97, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        
+        shapeLayerBreakTimer.path = circularPatch.cgPath
+        shapeLayerBreakTimer.lineWidth = 3.5
+        shapeLayerBreakTimer.fillColor = UIColor.clear.cgColor
+        shapeLayerBreakTimer.strokeEnd = 1
+        shapeLayerBreakTimer.lineCap = CAShapeLayerLineCap.round
+        shapeLayerBreakTimer.strokeColor = UIColor(named: "TimerColorBreak")!.cgColor
+        cercleTimerBreakImage.layer.addSublayer(shapeLayerBreakTimer)
     }
     
     func basicAnimation() {
@@ -181,7 +250,8 @@ class ViewController: UIViewController {
         basicAnimation.duration = CFTimeInterval(durationTimer)
         basicAnimation.fillMode = CAMediaTimingFillMode.forwards
         basicAnimation.isRemovedOnCompletion = true
-        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+        shapeLayerWorkTimer.add(basicAnimation, forKey: "basicAnimation")
+        shapeLayerBreakTimer.add(basicAnimation, forKey: "basicAnimation")
     }
 }
 
@@ -194,34 +264,59 @@ extension ViewController {
             scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             scrollView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 320),
             scrollView.heightAnchor.constraint(equalToConstant: 380),
-            scrollView.widthAnchor.constraint(equalToConstant: view.frame.size.width)
         ])
         
-        view.addSubview(cercleTimer)
+        viewTimerWorked.addSubview(cercleTimerWorkImage)
         NSLayoutConstraint.activate([
-            cercleTimer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            cercleTimer.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 320),
-            cercleTimer.heightAnchor.constraint(equalToConstant: 380),
-            cercleTimer.widthAnchor.constraint(equalToConstant: 380)
+            cercleTimerWorkImage.centerXAnchor.constraint(equalTo: viewTimerWorked.centerXAnchor),
+            cercleTimerWorkImage.centerYAnchor.constraint(equalTo: viewTimerWorked.centerYAnchor),
+            cercleTimerWorkImage.heightAnchor.constraint(equalToConstant: 400),
+            cercleTimerWorkImage.widthAnchor.constraint(equalToConstant: 400)
+        ])
+ 
+        cercleTimerWorkImage.addSubview(timerWorkLabel)
+        NSLayoutConstraint.activate([
+            timerWorkLabel.centerXAnchor.constraint(equalTo: cercleTimerWorkImage.centerXAnchor),
+            timerWorkLabel.centerYAnchor.constraint(equalTo: cercleTimerWorkImage.centerYAnchor)
         ])
         
-        cercleTimer.addSubview(timerLabel)
+        viewTimerWorked.addSubview(workLabel)
         NSLayoutConstraint.activate([
-            timerLabel.centerXAnchor.constraint(equalTo: cercleTimer.centerXAnchor),
-            timerLabel.centerYAnchor.constraint(equalTo: cercleTimer.centerYAnchor)
+            workLabel.centerXAnchor.constraint(equalTo: viewTimerWorked.centerXAnchor),
+            workLabel.centerYAnchor.constraint(equalTo: viewTimerWorked.bottomAnchor)
+        ])
+        
+        viewTimerBreak.addSubview(cercleTimerBreakImage)
+        NSLayoutConstraint.activate([
+            cercleTimerBreakImage.centerXAnchor.constraint(equalTo: viewTimerBreak.centerXAnchor),
+            cercleTimerBreakImage.centerYAnchor.constraint(equalTo: viewTimerBreak.centerYAnchor),
+            cercleTimerBreakImage.heightAnchor.constraint(equalToConstant: 400),
+            cercleTimerBreakImage.widthAnchor.constraint(equalToConstant: 400)
+        ])
+        
+        cercleTimerBreakImage.addSubview(timerBreakLabel)
+        NSLayoutConstraint.activate([
+            timerBreakLabel.centerXAnchor.constraint(equalTo: cercleTimerBreakImage.centerXAnchor),
+            timerBreakLabel.centerYAnchor.constraint(equalTo: cercleTimerBreakImage.centerYAnchor)
+        ])
+        
+        viewTimerBreak.addSubview(breakLabel)
+        NSLayoutConstraint.activate([
+            breakLabel.centerXAnchor.constraint(equalTo: viewTimerBreak.centerXAnchor),
+            breakLabel.centerYAnchor.constraint(equalTo: viewTimerBreak.bottomAnchor)
         ])
         
         view.addSubview(pageControl)
         NSLayoutConstraint.activate([
-            pageControl.centerXAnchor.constraint(equalTo: cercleTimer.centerXAnchor),
-            pageControl.topAnchor.constraint(equalTo: cercleTimer.bottomAnchor, constant: 0),
-            pageControl.heightAnchor.constraint(equalToConstant: 50),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.topAnchor.constraint(equalTo: view.topAnchor, constant: 530),
+            pageControl.heightAnchor.constraint(equalToConstant: 25),
             pageControl.widthAnchor.constraint(equalToConstant: 200)
         ])
         
         view.addSubview(startButton)
         NSLayoutConstraint.activate([
-            startButton.centerXAnchor.constraint(equalTo: cercleTimer.centerXAnchor),
+            startButton.centerXAnchor.constraint(equalTo: pageControl.centerXAnchor),
             startButton.topAnchor.constraint(equalTo: pageControl.bottomAnchor, constant: 0),
             startButton.heightAnchor.constraint(equalToConstant: 120),
             startButton.widthAnchor.constraint(equalToConstant: 120)
